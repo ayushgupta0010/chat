dd1431b433e5c9c1a3ef299ede6e5ac1d5d119f6
Ayush Gupta <ayushgupta0010@gmail.com>
2021-08-20 17:54:46 +0530

    Added React

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+
+const App = () => <BrowserRouter></BrowserRouter>;
+
+export default App;
