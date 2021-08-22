d2958a0c6a27143e716abc08661be6055bec44f9
Ayush Gupta <ayushgupta0010@gmail.com>
2021-08-23 01:38:42 +0530

    Added attachments option

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -3,6 +3,13 @@ import uuid
 from django.contrib.auth.models import User
 from django.db import models
 
+FILE_TYPES = [
+    ('audio', 'audio'),
+    ('document', 'document'),
+    ('image', 'image'),
+    ('video', 'video'),
+]
+
 
 class Group(models.Model):
     uuid = models.UUIDField(editable=False, default=uuid.uuid4, primary_key=True)
@@ -25,10 +32,12 @@ class Contact(models.Model):
 
 
 class Chat(models.Model):
+    message = models.TextField()
+    file_type = models.CharField(max_length=8, choices=FILE_TYPES, blank=True, null=True)
+    files = models.JSONField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
     sender = models.ForeignKey(User, on_delete=models.CASCADE)
     group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='chat')
-    message = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.sender}'
